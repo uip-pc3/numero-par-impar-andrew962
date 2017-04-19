5e90d645d8d9a11ed7bbdfbb1a1143a34de32f6e
Unknown <abadia962@gmail.com>
2017-04-19 14:29:44 -0500

    Cambios

--- App/main.py
+++ App/main.py
@@ -6,10 +6,11 @@ def num(n1):
         print('Numero Impar')
         return 'Numero Impar'
 """
-:type n1: int
-:type r: int
-:var n1: es el cambio de nombre que sele la a la variable n del main
-:var r: solo era para ver el resultado
+:param: todos:Prueba
+:type: n1 int
+:type: r int
+:var: n1 es el cambio de nombre que sele la a la variable n del main
+:var: r solo era para ver el resultado
 """
 
 
@@ -17,7 +18,7 @@ if __name__=="__main__":
     n = int(input('Introdusca numero: '))
     num(n)
     """
-    :type n: int
-    :var n: Se esta utilizando para guardar el numero introducidoy pasarlo a la funcion
-    :param num(): funcion llamada para hacer el Calculo
+    :type: n int
+    :var: n Se esta utilizando para guardar el numero introducidoy pasarlo a la funcion
+    :param: num() funcion llamada para hacer el Calculo
     """
---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndic":0,busqued:0,modul:0,pagin:0},titles:["Welcome to parimpar&#8217;s documentation!"],titleterms:{and:0,documentation:0,indic:0,parimp:0,tabl:0,welcom:0}})
